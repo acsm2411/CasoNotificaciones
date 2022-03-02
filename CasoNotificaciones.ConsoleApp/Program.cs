f4262e8b5d6457afbce7ed4c7c467d5856d37acc
Andres Silva <Andres.Silva@anuntatech.com>
2022-03-01 19:25:53 -0500

    Missing change

--- CasoNotificaciones.ConsoleApp/Program.cs
+++ CasoNotificaciones.ConsoleApp/Program.cs
@@ -117,7 +117,7 @@ Mensaje ObtenerDatosDeMensaje()
                 Console.WriteLine("\nEl destinatario no puede estar vacio, ingrese la informacion requerida");
             }
         }
-        while (destinatario.Length == 0);
+        while (string.IsNullOrEmpty(destinatario));
 
         mensaje.Destinatarios.Add(destinatario);
 
